b5abfe3a45e194db49cd2704057601554522f0bb
neal1991 <neal1991@sina.com>
2017-09-03 10:23:27 +0800

    v0.1.8 :rocket:

--- pages/free/index.js
+++ pages/free/index.js
@@ -4,71 +4,74 @@ const data = require('../../libs/data.js');
 Page({
   data: {
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    socialData: data.socialData(),
     index1: 0,
     freeData: data.freeData(),
     tableInfo1: data.freeData()['黄浦区'],
-    district1: ['黄浦区', '静安区', '嘉定区', '浦东新区', '徐汇区', '虹口区', '长宁区', '杨浦区', '闵行区', '宝山区', '奉贤区', '崇明区'],
+    district1: ['黄浦区', '静安区', '嘉定区', '浦东新区', '徐汇区', '虹口区', '长宁区', '杨浦区', '闵行区', '宝山区', '奉贤区', '崇明区', '普陀区'],
     objectDistrict: [
       {
         id: 0,
         name: '黄浦区'
       },
       {
-        id: 0,
+        id: 1,
         name: '静安区'
       },
       {
-        id: 0,
+        id: 2,
         name: '嘉定区'
       },
       {
-        id: 0,
+        id: 3,
         name: '浦东新区'
       },
       {
-        id: 0,
+        id: 4,
         name: '徐汇区'
       },
       {
-        id: 0,
+        id: 5,
         name: '虹口区'
       },
       {
-        id: 0,
+        id: 6,
         name: '长宁区'
       },
       {
-        id: 0,
+        id: 7,
         name: '杨浦区'
       },
       {
-        id: 0,
+        id: 8,
         name: '闵行区'
       },
       {
-        id: 0,
+        id: 9,
         name: '宝山区'
       },
       {
-        id: 0,
+        id: 10,
         name: '奉贤区'
       },
       {
-        id: 0,
+        id: 11,
         name: '崇明区'
+      },
+      {
+        id: 12,
+        name: '普陀区'
       }
     ]
   },
   //事件处理函数
-  // bindViewTap: function () {
-  //   wx.navigateTo({
-  //     url: '../logs/logs',
-  //     success: function (res) {
-  //       console.log(res.data);
-  //     }
-  //   })
-  // },
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs',
+      success: function (res) {
+        console.log(res.data);
+      }
+    })
+  },
   bindPickerChange1: function (e) {
     this.setData({
       index1: e.detail.value,
@@ -76,6 +79,5 @@ Page({
     })
   },
   onLoad: function () {
-
   },
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,69 +1,67 @@
 //index.js
 //获取应用实例
-const app = getApp()
 const data = require('../../libs/data.js');
-const baseUrl = 'https://api.map.baidu.com/geocoder/v2/?address=北京市海淀区上地十街10号&output=json&ak=qMuA8fTWTM4AwtdP4uGa0ZHqzGOj18LP'
-const url = 'https://api.map.baidu.com/routematrix/v2/riding?output=json&origins=40.45,116.34&destinations=40.34,116.456&ak=qMuA8fTWTM4AwtdP4uGa0ZHqzGOj18LP'
+var baseUrl = 'http://api.map.baidu.com/routematrix/v2/riding?output=json&';
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
     index: 0,
-    hasUserInfo: false,
-    geoInfo: {},
+    location: {},
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     socialData: data.socialData(),
-    freeData: data.freeData(),
     tableInfo: data.socialData()['黄浦区'],
-    district: ['黄浦区', '静安区', '嘉定区', '浦东新区', '徐汇区', '虹口区', '长宁区', '杨浦区','闵行区', '宝山区', '奉贤区', '崇明区'],
+    district: ['黄浦区', '静安区', '嘉定区', '浦东新区', '徐汇区', '虹口区', '长宁区', '杨浦区','闵行区', '宝山区', '奉贤区', '崇明区', '普陀区'],
     objectDistrict: [
       {
         id: 0,
         name: '黄浦区'
       },
       {
-        id: 0,
+        id: 1,
         name: '静安区'
       },
       {
-        id: 0,
+        id: 2,
         name: '嘉定区'
       },
       {
-        id: 0,
+        id: 3,
         name: '浦东新区'
       },
       {
-        id: 0,
+        id: 4,
         name: '徐汇区'
       },
       {
-        id: 0,
+        id: 5,
         name: '虹口区'
       },
       {
-        id: 0,
+        id: 6,
         name: '长宁区'
       },
       {
-        id: 0,
+        id: 7,
         name: '杨浦区'
       },
       {
-        id: 0,
+        id: 8,
         name: '闵行区'
       },
       {
-        id: 0,
+        id: 9,
         name: '宝山区'
       },
       {
-        id: 0,
+        id: 10,
         name: '奉贤区'
       },
       {
-        id: 0,
+        id: 11,
         name: '崇明区'
+      },{
+        id: 12,
+        name: '普陀区'
       }
     ]
   },
@@ -85,64 +83,6 @@ Page({
   onLoad: function () {
     this.setData({
       tableInfo: this.data.socialData['黄浦区']
-    })
-    // wx.getLocation({
-    //   success: function(res) {
-    //     var geoInfo = {
-    //       latitude: res.latitude,
-    //       longitude: res.longitude
-    //     };
-    //     // this.setData({
-    //     //   geoInfo: geoInfo
-    //     // })
-    //   },
-    // })
-    // wx.request({
-    //   url: url,
-    //   success: function(res) {
-    //     console.log(res.data);
-    //   }
-    // })
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-  getGeoInfo: function() {
-    wx.getLocation({
-      success: function(res) {
-        console.log(res);
-      },
-    })
+    })     
   }
 })
