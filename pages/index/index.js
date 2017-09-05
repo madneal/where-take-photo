de2ebc73a037a74bbfe7f6c49aec5e1a90a15466
neal1991 <neal1991@sina.com>
2017-09-05 20:00:01 +0800

    add info page :sparkles:

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,60 +11,7 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     socialData: data.socialData(),
     tableInfo: data.socialData()['黄浦区'],
-    district: ['黄浦区', '静安区', '嘉定区', '浦东新区', '徐汇区', '虹口区', '长宁区', '杨浦区','闵行区', '宝山区', '奉贤区', '崇明区', '普陀区'],
-    objectDistrict: [
-      {
-        id: 0,
-        name: '黄浦区'
-      },
-      {
-        id: 1,
-        name: '静安区'
-      },
-      {
-        id: 2,
-        name: '嘉定区'
-      },
-      {
-        id: 3,
-        name: '浦东新区'
-      },
-      {
-        id: 4,
-        name: '徐汇区'
-      },
-      {
-        id: 5,
-        name: '虹口区'
-      },
-      {
-        id: 6,
-        name: '长宁区'
-      },
-      {
-        id: 7,
-        name: '杨浦区'
-      },
-      {
-        id: 8,
-        name: '闵行区'
-      },
-      {
-        id: 9,
-        name: '宝山区'
-      },
-      {
-        id: 10,
-        name: '奉贤区'
-      },
-      {
-        id: 11,
-        name: '崇明区'
-      },{
-        id: 12,
-        name: '普陀区'
-      }
-    ]
+    district: ['黄浦区', '静安区', '嘉定区', '浦东新区', '徐汇区', '虹口区', '长宁区', '杨浦区','闵行区', '宝山区', '奉贤区', '崇明区', '普陀区', '松江区']
   },
   //事件处理函数
   bindViewTap: function() {
@@ -80,6 +27,11 @@ Page({
     console.dir(e);
     wx.makePhoneCall({
       phoneNumber: tel,
+    })
+  },
+  navigateinfo: function() {
+    wx.navigateTo({
+      url: '../info/index',
     })
   },
   openmap: function(e) {
